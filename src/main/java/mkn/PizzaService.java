cbefe95059cb24f3bdee1f970800765f566d7b62
mknutty <6239021+mknutty@users.noreply.github.com>
2023-04-06 13:59:49 -0400

    Update PizzaService.java

--- src/main/java/mkn/PizzaService.java
+++ src/main/java/mkn/PizzaService.java
@@ -1,5 +1,9 @@
 package mkn;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,24 +13,35 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class PizzaService {
-	private PizzaRepository repository;
+	private final PizzaRepository repository;
 	private final ApplicationEventPublisher applicationEventPublisher;
-	
+
 	@Transactional
-	public Pizza create(String orderedBy, String type) {
-		Pizza pizza = repository.save(new Pizza().setOrderedBy(orderedBy).setType(type));
+	public Pizza create(final String orderedBy, final String type) {
+		final Pizza pizza = repository.save(new Pizza().setOrderedBy(orderedBy).setType(type));
 		applicationEventPublisher.publishEvent(new PizzaCreatedEvent(pizza.getId()));
 		return pizza;
 	}
-	
+
 	@Transactional
-	public void delete(Long id) {
+	public void delete(final Long id) {
 		repository.deleteById(id);
-		applicationEventPublisher.publishEvent(new PizzaDeletedEvent(id ));
+		applicationEventPublisher.publishEvent(new PizzaDeletedEvent(id));
 	}
 
-  public void update(Long id, String type) {
-    repository.save(repository.findById(id).get().setType(type));
-    
-  }
+	public void update(final Long id, final String type) {
+		repository.save(repository.findById(id).get().setType(type));
+
+	}
+	
+	@Transactional
+	public List<Pizza> create2(final String orderedBy, final String... types) {
+		final List<Pizza> pizzas = new ArrayList<>();
+		Arrays.asList(types).forEach(type -> {
+			final Pizza pizza = repository.save(new Pizza().setOrderedBy(orderedBy).setType(type));
+			pizzas.add(pizza);
+		});
+		
+		return pizzas;
+	}
 }
